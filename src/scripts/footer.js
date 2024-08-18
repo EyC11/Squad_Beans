06606c29288b022cd4d8dc741194d46869899b43
Jorge Jimenez <jjforce13@gmail.com>
2024-08-17 18:47:50 -0600

    -- Todos los documentos que no fueran login

--- src/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-//   mostrar el footer al llegar al final de la página
-window.addEventListener('scroll', function() {
-    const footer = document.getElementById('footer');
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      footer.classList.add('visible');
-    } else {
-      footer.classList.remove('visible');
-    }
-  });
