378712235fa16b96393434437fcc76d5b1098407
Jorge Jimenez <jjforce13@gmail.com>
2024-08-24 14:24:46 -0600

    Actualización página Donativos

--- src/scripts/donativos.js
+++ src/scripts/donativos.js
@@ -2,6 +2,8 @@
 const donationButtons = document.querySelectorAll('.btn-donation');
 const manualInput = document.getElementById('input-donaciones');
 const donationAmountDisplay = document.getElementById('donaciones-amount');
+const paymentForm = document.getElementById('paymentForm');
+const cancelButton = document.getElementById('pago--btnCancelar');
 
 // Función para actualizar el monto de donaciones
 function updateDonationAmount(amount) {
@@ -25,3 +27,33 @@ manualInput.addEventListener('input', () => {
         updateDonationAmount('0');
     }
 });
+
+// Manejar el evento de envío del formulario de pago
+paymentForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío real del formulario
+
+    // Obtener los valores de los campos
+    const email = document.getElementById('email').value.trim();
+    const cardNumber = document.getElementById('cardNumber').value.trim();
+    const expiryDate = document.getElementById('expiryDate').value.trim();
+    const cvv = document.getElementById('cvv').value.trim();
+    const fullName = document.getElementById('fullName').value.trim();
+    const country = document.getElementById('country').value.trim();
+    const billingAddress = document.getElementById('billingAddress').value.trim();
+
+    // Verificar si todos los campos están llenos
+    if (email && cardNumber && expiryDate && cvv && fullName && country && billingAddress) {
+        alert('Gracias!! c: Tu donación se ha enviado correctamente!');
+        serviceForm.reset();
+        paymentForm.reset();
+        
+    } else {
+        alert('Por favor, complete todos los campos del formulario.');
+    }
+});
+
+    //El usuario decide cancelar 
+    cancelButton.addEventListener('click', function() {
+        paymentForm.reset(); // Restablecer el formulario de pago
+        serviceForm.reset(); // Restablecer el formulario de donación (si es necesario)
+    });
